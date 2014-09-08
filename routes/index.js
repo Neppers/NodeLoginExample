caf718799ff208dc8b1c31a44f6a535e401786ac
Nick Price <neppers+git@gmail.com>
2014-09-08 15:01:33 +0100

    Login POST

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
 
 /* Redirect home page to login */
 router.get('/', function(req, res) {
     res.redirect('/login');
 });
 
-/* GET home page. */
+/* GET login */
 router.get('/login', function(req, res) {
   res.render('index', { title: 'Node Login Example w/ Passport' });
 });
+
+/* POST login */
+router.post('/login', passport.authenticate('local-login', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true,
+    badRequestMessage: 'You must specify an email address and password'
+}));
 
 module.exports = router;
