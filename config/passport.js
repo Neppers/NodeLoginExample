b9c430137f51e587591bcea29c0c9eb79566434f
Nick Price <neppers+git@gmail.com>
2014-09-08 14:36:41 +0100

    Passport config 4

--- config/passport.js
+++ config/passport.js
@@ -26,9 +26,17 @@ module.exports = function(passport) {
             if (password !== req.body.confirmPassword)
                 return done(null, false, { message: 'Passwords do not match' });
             if (user) {
-                //4. If user already exists -> throw error    
+                //4. If user already exists -> throw error
+                return done(null, false, { message: 'Email address is already in use' });
             } else {
                 //5. If no errors, create a new user
+                var newUser = new User();
+                newUser.local.email = email;
+                newUser.local.password = newUser.generateHash(password);
+                newUser.save(function(err) {
+                    if (err) throw err;
+                    return done(null, newUser);
+                });
             }
         });        
     }));
