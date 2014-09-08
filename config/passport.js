bbb4147a8fc45f2d23b232c420bb75727e943037
Nick Price <neppers+git@gmail.com>
2014-09-08 14:20:38 +0100

    Passport config 2

--- config/passport.js
+++ config/passport.js
@@ -1,2 +1,38 @@
 var LocalStrategy = require('passport-local').Strategy;
 var User = require('../models/user');
+
+module.exports = function(passport) {
+    passport.serializeUser(function(user, done) {
+        done(null, user.id);
+    });
+    
+    passport.deserializeUser(function(id, done) {
+        User.findById(id, function(err, user) {
+            done(err, user);
+        });
+    });
+    
+    passport.use('local-signup', new LocalStrategy({
+        usernameField: 'email',
+        passwordField: 'password',
+        passReqToCallback: true
+    }, function(req, email, password, done) {
+        
+        /*
+        Here is where we create a new user
+         */
+        
+    }));
+    
+    passport.use('local-login', new LocalStrategy({
+        usernameField: 'email',
+        passwordField: 'password',
+        passReqToCallback: true
+    }, function(req, email, password, done) {
+        
+        /*
+        Here is where we login our user in
+         */
+        
+    }));
+};
